dfaf2f9b867ddee2605206b3f45911450295b391
Rangaramanujam N S <rangaramanujamsri@gmail.com>
2024-05-08 16:45:00 +0000

    commit#

--- ranga-stt-ai-app/src/app/layout.tsx
+++ ranga-stt-ai-app/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-
+import { SpeedInsights } from "@vercel/speed-insights/next"
+import { Analytics } from "@vercel/analytics/react"
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
